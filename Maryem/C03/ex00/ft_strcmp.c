f55741c4b47e43e25c87c1b6ace752fbd4f6fde0
MeryamAj <maryamajgagale826@gmail.com>
2026-04-10 20:44:35 +0100

    C02

---
+++ Maryem/C03/ex00/ft_strcmp.c
@@ -0,0 +1,23 @@
+#include<unistd.h>
+#include<stdio.h>
+int ft_strcmp(char *s1, char *s2)
+{
+    int i = 0 ;
+    while(s1[i] != '\0' || s2[i] != '\0')
+    {
+        if(s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+        i++;
+    }
+    return 0;
+}
+int main()
+{
+    char s1[] = "abc";
+    char s2[] = "abc";
+    int r = ft_strcmp(s1,s2);
+    printf("%d\n",r);
+    return 0;
+}
