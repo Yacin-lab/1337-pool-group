5c40b0d6ec40a304f26b4da753a1a7a510f49fc9
Yacin-lab <baadiyassine51@gmail.com>
2026-05-11 12:43:49 +0100

    update

--- Yassine/C04/ex01/ft_putstr.c
+++ Yassine/C04/ex01/ft_putstr.c
@@ -6,11 +6,16 @@
 /*   By: ybaadi <ybaadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 13:12:55 by ybaadi            #+#    #+#             */
-/*   Updated: 2026/05/05 12:22:30 by ybaadi           ###   ########.fr       */
+/*   Updated: 2026/05/11 00:00:06 by ybaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void    ft_putstr(char *str)
 {
@@ -19,7 +24,7 @@ void    ft_putstr(char *str)
     i = 0;
     while (str[i])
     {
-        write(1, (str + i), 1);
+        ft_putchar(str[i]);
         i++;
     }
 }
